f1582ba1fd98e2ca6c807e424d3bf895255f4b24
Xander <j-xander@users.noreply.github.com>
2016-12-20 00:14:53 -0500

    Update startupScript.sh
    Added correct indents

--- startupScript.sh
+++ startupScript.sh
@@ -6,29 +6,29 @@ int="en0";
 
 for i in {1..12}; do
 
-rand=$(( ( RANDOM % 15 )  + 1 ));
+	rand=$(( ( RANDOM % 15 )  + 1 ));
 
-if (( count % 2 == 0 && count != 0)); then
-	mac+=":";
-fi
+	if (( count % 2 == 0 && count != 0)); then
+		mac+=":";
+	fi
 
-if [ $rand -eq 10 ]; then 
-	mac+='a';
-elif [ $rand -eq 11 ]; then
-	mac+='b';
-elif [ $rand -eq 12 ]; then
-        mac+='c';
-elif [ $rand -eq 13 ]; then
-        mac+='d';
-elif [ $rand -eq 14 ]; then
-        mac+='e';
-elif [ $rand -eq 15 ]; then
-        mac+='f';
-else
-	mac+=$rand;
-fi
+	if [ $rand -eq 10 ]; then 
+		mac+='a';
+	elif [ $rand -eq 11 ]; then
+		mac+='b';
+	elif [ $rand -eq 12 ]; then
+        	mac+='c';
+	elif [ $rand -eq 13 ]; then
+        	mac+='d';
+	elif [ $rand -eq 14 ]; then
+        	mac+='e';
+	elif [ $rand -eq 15 ]; then
+        	mac+='f';
+	else
+		mac+=$rand;
+	fi
 
-((count++));
+	((count++));
 
 done
 
